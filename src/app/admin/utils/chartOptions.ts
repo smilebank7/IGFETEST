fa7d3664a3d468567d5a9a4b7ab9d3b6e1594e25
이주형 JuhyeongLee <smilebank7@naver.com>
2025-02-18 09:31:14 +0900

    feat : add animation in admin page stats

--- src/app/admin/utils/chartOptions.ts
+++ src/app/admin/utils/chartOptions.ts
@@ -5,22 +5,30 @@ const baseOptions = {
   maintainAspectRatio: true,
   aspectRatio: 2,
   resizeDelay: 100,
-  animation: { duration: 0 }
 }
 
 export const barChartOptions: ChartOptions<'bar'> = {
   ...baseOptions,
-  plugins: { legend: { position: 'top' } }
+  plugins: { legend: { position: 'top' } },
+  animation: {
+    duration: 1000
+  }
 }
 
 export const pieChartOptions: ChartOptions<'pie'> = {
   ...baseOptions,
   layout: { padding: { left: 80, right: 80, top: 30, bottom: 10 } },
-  plugins: { legend: { position: 'right', align: 'center' } }
+  plugins: { legend: { position: 'right', align: 'center' } },
+  animation: {
+    duration: 1000
+  }
 }
 
 export const radarChartOptions: ChartOptions<'radar'> = {
   ...baseOptions,
   layout: { padding: { left: 80, right: 80, top: 30, bottom: 10 } },
-  plugins: { legend: { position: 'right', align: 'center' } }
+  plugins: { legend: { position: 'right', align: 'center' } },
+  animation: {
+    duration: 1000
+  }
 } 
